c6728c27932c60becf535551b6aa32f16733a857
Divyansh sahu <divyanshsahu80@gmail.com>
2021-06-06 00:47:52 +0530

    Defined mutators for the process class

--- src/Process.cpp
+++ src/Process.cpp
@@ -13,15 +13,6 @@ std::string Process::getProcess()
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   "
-                    + this->user
-                    + "   "
-                    + this->mem.substr(0,5)
-                    + "     "
-                    + this->cpu.substr(0,5)
-                    + "     "
-                    + this->upTime.substr(0,5)
-                    + "    "
-                    + this->cmd.substr(0,30)
-                    + "...");
+    std::string res = (this->pid + "   " + this->user + "   " + this->mem.substr(0,5) + "     " + this->cpu.substr(0,5) + "     " + this->upTime.substr(0,5) + "    " + this->cmd.substr(0,30) + "...");
+    return res;
 }
